59530ff80dbfd36b37d456d4ec02bddbd5de0efc
Rohan Dudeja <rohanddj@gmail.com>
2020-02-18 17:27:15 +0530

    Update app.R

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,3 +1,12 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -16,7 +25,7 @@ ui <- fluidPage(
             # Input: Numeric entry for number of obs to view ----
             numericInput(inputId = "penalty",
                          label = "Penalty parameter",
-                         value = 10)
+                         value = 1)
         ),
         # Show a plot of the generated distribution
         mainPanel(
