3a70d0cff1269191ff336024ef8575c595b0f995
Rohan Dudeja <rohanddj@gmail.com>
2020-02-19 14:04:26 +0530

    Update rdriver.R

--- d3task/rdriver.R
+++ d3task/rdriver.R
@@ -2,4 +2,5 @@ library("r2d3")
 x<-seq(1, 100)
 y<-pnorm(x, mean = 60, sd = 20)
 d<-data.frame(x,y)
-r2d3(data = d,script = "https://raw.githubusercontent.com/RohanDudeja/guiTask/master/d3task/d3task.js")
+r2d3(data = d,script = "d3task.js")
+
